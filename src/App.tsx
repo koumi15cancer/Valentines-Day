2c8e992dc47a9ba3dd6a711693b59761f05f2736
koumi15cancer <daiduong040700@gmail.com>
2024-02-14 08:09:57 +0800

    finish just fun thing for valentine

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ export default function Page() {
   const [yesPressed, setYesPressed] = useState(false);
   const yesButtonSize = noCount * 20 + 16;
 
+
+
   const handleNoClick = () => {
     setNoCount(noCount + 1);
   };
@@ -13,58 +15,86 @@ export default function Page() {
   const getNoButtonText = () => {
     const phrases = [
       "No",
-      "Are you sure?",
-      "Really sure?",
-      "Think again!",
-      "Last chance!",
-      "Surely not?",
-      "You might regret this!",
-      "Give it another thought!",
-      "Are you absolutely certain?",
-      "This could be a mistake!",
-      "Have a heart!",
-      "Don't be so cold!",
-      "Change of heart?",
-      "Wouldn't you reconsider?",
-      "Is that your final answer?",
-      "You're breaking my heart ;(",
+      "Chắc Humm?",
+      "Chắc chắn Humm?",
+      "Chọn lại oii!",
+      "Chọn sai oi! Lại nữa oii",
+      "Bấm lộn òii, Bấm lại coii?",
+      "Chọn lại coii hong hết cơ hội",
+      "Lại bấm lộn òii kìaa, nút màu xanh  cơ màa!",
+      "Bấm lộn là hết cơ hội đó, nút màu xanh óo",
+      "Cậu thấy đóo, chỉ có 1 lựa chọnn!",
+      "Cậu có thấy nút nào giờ to quá hemm!",
+      "Nút màu xanh đi òo!",
+      "Suy nghĩ kĩ lại xemm",
+      "Cân nhắc lại oii?",
+      "Thiệt hỏo?",
+      "Bùnn lémm",
     ];
 
     return phrases[Math.min(noCount, phrases.length - 1)];
   };
 
+  function getRandomPosition(): { x: number, y: number } {
+    const x = Math.random() * window.innerWidth;
+    const y = Math.random() * window.innerHeight;
+    return { x, y };
+  }
+  
+  // Function to update the position of all GIFs
+  function updateGIFsPosition(): void {
+    const gifs = document.querySelectorAll('.gif') as NodeListOf<HTMLImageElement>;
+    gifs.forEach(gif => {
+      const position = getRandomPosition();
+      gif.style.left = `${position.x}px`;
+      gif.style.top = `${position.y}px`;
+    });
+  }
+  
+  // Update the position of GIFs initially
+  updateGIFsPosition();
+  
+  // Update the position of GIFs every 3 seconds (adjust as needed)
+  setInterval(updateGIFsPosition, 3000);
+
   return (
+    
     <div className="centered-container">      
       <div className="valentine-container">
         {yesPressed ? (
           <>
-            <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
-            <div className="text-container">Ok yay!!!</div>
+            <img src="https://media.tenor.com/OdsUDG_5TrwAAAAi/mamegoma-baby.gif" />
+            <div className="text-container">So u chose Yes!!! Hehe</div>
           </>
         ) : (
           <>
             <img
               className="h-[200px]"
               style={{ width: "400x", height: "240px" }}
-              src="https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.gif"
+              src="https://media.tenor.com/Fk25B5wkHSEAAAAi/mamegoma-baby.gif"
             />
-            <h1 className="text-container">Will you be my Valentine?</h1>
+            <h1 className="text-container">Will you "Đứm" me on this Valentine?</h1>
             <div>
               <button
                 className={"yes-button"}
                 style={{ fontSize: yesButtonSize }}
+                
                 onClick={() => setYesPressed(true)}
               >
-                Yes
+             Yes
+                
               </button>
 
               <button onClick={handleNoClick} className="no-button">
                 {noCount === 0 ? "No" : getNoButtonText()}
               </button>
+              
             </div>
+            
           </>
         )}
       </div>
+      <img className="gif" src="https://media.tenor.com/temsz9f6WucAAAAj/mamegoma-group.gif" alt="GIF 1" />
     </div>
   );
 }
